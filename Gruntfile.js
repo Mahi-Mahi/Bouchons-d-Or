9eabc5ee311f6589238a5a86c5f25a6154fd9141
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2014-09-01 20:19:31 +0200

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,202 @@
+/*
+ * grunt-init
+ * https://gruntjs.com/
+ *
+ * Copyright (c) 2014 "Cowboy" Ben Alman, contributors
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  var appConfig = {
+    app: require('./bower.json').appPath || 'app',
+    dist: '../web'
+  };
+
+  // Project configuration.
+  grunt.initConfig({
+
+    appConfig: appConfig,
+
+    watch: {
+      bower: {
+        files: ['bower.json'],
+        tasks: ['wiredep']
+      },
+      js: {
+        files: ['<%= appConfig.app %>/scripts/{,*/}*.js'],
+        tasks: ['newer:jshint:all'],
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        }
+      },
+      compass: {
+        files: ['<%= appConfig.app %>/styles/{,*/}*.{scss,sass}'],
+        tasks: ['compass:server', 'autoprefixer']
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= appConfig.app %>/{,*/}*.html',
+          '.tmp/styles/{,*/}*.css',
+          '<%= appConfig.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+
+    // Compiles Sass to CSS and generates necessary files if requested
+    compass: {
+      options: {
+        sassDir: '<%= appConfig.app %>/styles',
+        cssDir: '.tmp/styles',
+        generatedImagesDir: '.tmp/images/generated',
+        imagesDir: '<%= appConfig.app %>/images',
+        javascriptsDir: '<%= appConfig.app %>/scripts',
+        fontsDir: '<%= appConfig.app %>/styles/fonts',
+        importPath: './bower_components',
+        httpImagesPath: '/images',
+        httpGeneratedImagesPath: '/images/generated',
+        httpFontsPath: '/styles/fonts',
+        relativeAssets: false,
+        assetCacheBuster: false,
+        raw: 'Sass::Script::Number.precision = 10\n'
+      },
+      dist: {
+        options: {
+          generatedImagesDir: '<%= appConfig.dist %>/images/generated'
+        }
+      },
+      server: {
+        options: {
+          debugInfo: true
+        }
+      }
+    },
+
+    // Add vendor prefixed styles
+    autoprefixer: {
+      options: {
+        browsers: ['last 1 version']
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/styles/',
+          src: '{,*/}*.css',
+          dest: '.tmp/styles/'
+        }]
+      }
+    },
+
+    jshint: {
+      all: [
+        'Gruntfile.js',
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9000,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost',
+        livereload: 35729
+      },
+      livereload: {
+        options: {
+          open: true,
+          middleware: function(connect) {
+            return [
+              connect.static('.tmp'),
+              connect().use(
+                '/bower_components',
+                connect.static('./bower_components')
+              ),
+              connect.static(appConfig.app)
+            ];
+          }
+        }
+      },
+      dist: {
+        options: {
+          open: true,
+          base: '<%= appConfig.dist %>'
+        }
+      }
+    },
+
+    // Run some tasks in parallel to speed up the build process
+    concurrent: {
+      server: [
+        'compass:server'
+      ],
+      dist: [
+        'compass:dist',
+        'imagemin',
+        // 'svgmin'
+      ]
+    },
+
+    // Automatically inject Bower components into the app
+    wiredep: {
+      options: {
+        cwd: '<%= appConfig.app %>'
+      },
+      app: {
+        src: ['<%= appConfig.app %>/index.html'],
+        ignorePath: /..\//
+      },
+      sass: {
+        src: ['<%= appConfig.app %>/styles/{,*/}*.{scss,sass}'],
+        ignorePath: /(\.\.\/){1,2}bower_components\//
+      }
+    },
+
+    // Empties folders to start fresh
+    clean: {
+      dist: {
+        options: {
+          force: true
+        },
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= appConfig.dist %>/{,*/}*',
+            '!<%= appConfig.dist %>/.git*'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
+
+  });
+
+  grunt.registerTask('serve', 'Compile then start a connect web server', function(target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
+    }
+
+    grunt.task.run([
+      'clean',
+      'wiredep',
+      'concurrent:server',
+      'autoprefixer',
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+
+};
