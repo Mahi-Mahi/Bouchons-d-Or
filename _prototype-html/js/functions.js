3f67bcd7628062e06a4c1f5586067f6d92c439e5
Alexandre Cougard <alexandre.cougard@gmail.com>
2014-09-02 12:14:53 +0200

    Update prototype HTML

---
+++ _prototype-html/js/functions.js
@@ -0,0 +1,28 @@
+// DISABLE HOVER WHILE SCROLL
+var disableHoverOnScroll = function() {
+	var body = document.body,
+		timer,
+		addEvent = window.attachEvent || window.addEventListener;
+	addEvent('scroll', function() {
+		clearTimeout(timer);
+		if (!body.classList.contains('disable-hover')) {
+			body.classList.add('disable-hover');
+		}
+		timer = setTimeout(function() {
+			body.classList.remove('disable-hover');
+		}, 500);
+	}, false);
+};
+
+// FILTERS
+var filterSelection = function() {
+	if (jQuery('#container').length) {
+		jQuery('#container').mixItUp();
+	}
+};
+
+// DOCUMENT READY
+jQuery(document).ready(function() {
+	disableHoverOnScroll();
+	filterSelection();
+});
